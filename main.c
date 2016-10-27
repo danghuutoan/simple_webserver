1f86fccd0b211e22f37e575fe65fb1e5ff410284
c_toan.dang <tony.dang@terralogic.com>
2016-10-27 13:41:43 +0700

    Adding a request handler

--- main.c
+++ main.c
@@ -10,29 +10,29 @@
 
 
 char *header = 
-"\r\nHTTP/1.1 200 OK\r\n"
-"\r\nDate: Mon, 27 Jul 2009 12:28:53 GMT\r\n"
-"\r\nServer: Apache/2.2.14 (Win32)\r\n"
-"\r\nLast-Modified: Wed, 22 Jul 2009 19:15:56 GMT\r\n"
-"\r\nContent-Length: 100\r\n"
-"\r\nContent-Type: text/html\r\n"
-"\r\nConnection: Closed\r\n";
+"HTTP/1.1 200 OK\r\n"
+"Date: Mon, 27 Jul 2009 12:28:53 GMT\r\n"
+"Server: Apache/2.2.14 (Win32)\r\n"
+"Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT\r\n"
+"Content-Length: 100\r\n"
+"Content-Type: text/html\r\n"
+"Connection: Closed\r\n";
 
 char *html_txt =
-"\r\n<!DOCTYPE HTML>\r\n" 
-"\r\n<html>\r\n"
-"\r\n<body>\r\n"
-"\r\n<h1>Hello, World!</h1>\r\n"
-"\r\n</p><form method='get' action='setting'><label>SSID: </label><input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>\r\n"
-"\r\n</body>\r\n"
-"\r\n</html>\r\n";
+"<!DOCTYPE HTML>\r\n" 
+"<html>\r\n"
+"<body>\r\n"
+"<h1>Hello, World!</h1>\r\n"
+"</p><form method='get' action='setting'><label>SSID: </label><input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>\r\n"
+"</body>\r\n"
+"</html>\r\n";
 void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
 
-void web_socket_read(char **buffer, int fd)
+int web_socket_read(char **buffer, int fd)
 {
 	char *temp;
 	int  received_bytes = 0;
@@ -51,6 +51,7 @@ void web_socket_read(char **buffer, int fd)
 		printf("received_bytes %d\n", received_bytes);
 		if (n < 0) error("ERROR reading from socket");
 	}
+	return received_bytes;
 }
 
 int main(int argc, char *argv[])
@@ -58,6 +59,13 @@ int main(int argc, char *argv[])
 	int sockfd, newsockfd, portno;
 	socklen_t clilen;
 	char *buffer = NULL;
+	int recv_len = 0;
+	int request_len = 0;
+	char *request_str;
+	char *ptr;
+	char *request_protocol;
+	char *request_cmd;
+	char *request_data;
 
 	struct sockaddr_in serv_addr, cli_addr;
 
@@ -81,27 +89,40 @@ int main(int argc, char *argv[])
 
 	listen(sockfd,5);
 	clilen = sizeof(cli_addr);
-	newsockfd = accept(sockfd, 
+	while(1)
+	{
+		newsockfd = accept(sockfd, 
 	         (struct sockaddr *) &cli_addr, 
 	         &clilen);
 
-	if (newsockfd < 0) 
-	  error("ERROR on accept");
+		if (newsockfd < 0) 
+		  error("ERROR on accept");
 
-	web_socket_read(&buffer, newsockfd);
-	printf("Here is the message: %s\n",buffer);
-	free(buffer);
-	printf("%s\n", header);
-	// send(newsockfd, header, strlen(header), 0);
-	send(newsockfd, html_txt, strlen(html_txt), 0);
+		printf("waiting for request\n");
+		recv_len = web_socket_read(&buffer, newsockfd);
 
-	newsockfd = accept(sockfd, 
-         (struct sockaddr *) &cli_addr, 
-         &clilen);
-	web_socket_read(&buffer, newsockfd);
-	send(newsockfd, html_txt, strlen(html_txt), 0);
-	printf("Here is the message: %s\n",buffer);
-	free(buffer);
+		/* searching for the first \r\n string */
+  		ptr = strstr(buffer,"\r\n");
+  		request_len = ptr - buffer ;
+  		request_str = malloc(sizeof(char)*request_len +1);
+		memcpy(request_str, buffer, request_len);
+  		request_str[request_len] = 0;
+
+		request_cmd = strtok (request_str," ");
+
+		if(strcmp(request_cmd,"GET") == 0)
+		{
+			printf("%s\n", request_cmd);
+			request_data = strtok (NULL," ");
+			printf("%s\n", request_data);
+			request_protocol = strtok (NULL," ");
+			printf("%s\n", request_protocol);	
+		}
+		free(request_str);
+		free(buffer);
+		send(newsockfd, html_txt, strlen(html_txt), 0);
+	}
+
 	close(newsockfd);
 	close(sockfd);
 	return 0; 
